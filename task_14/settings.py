89f6ae3b65dc64b25342af4357614d031ce4a24c
mac <eng.abdullahshafaal@gmail.com>
2019-02-19 17:34:49 +0300

    shaafal

--- task_14/settings.py
+++ task_14/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+
     'restaurants',
     'api',
     'crispy_forms',
+    'rest_framework',
 ]
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
